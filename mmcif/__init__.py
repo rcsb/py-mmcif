cbf40c6d239476b1c4760b69d677859e00ccfb2f
jwest <john.westbrook@rcsb.org>
2020-05-09 18:46:29 -0400

    V052 - Make reserved token quoting case insensitive (PR from E. Ezra Peisach)

--- mmcif/__init__.py
+++ mmcif/__init__.py
@@ -2,6 +2,6 @@ __docformat__ = "restructuredtext en"
 __author__ = "John Westbrook"
 __email__ = "john.westbrook@rcsb.org"
 __license__ = "Apache 2.0"
-__version__ = "0.51"
+__version__ = "0.52"
 
 __apiUrl__ = "https://mmcif.wwpdb.org"
