29d06219efba8fa6bb86069375e685ac4903c40e
jwest <john.westbrook@rcsb.org>
2020-06-05 15:49:14 -0400

    V056 - Address Py27 compatibility issues

--- mmcif/__init__.py
+++ mmcif/__init__.py
@@ -2,6 +2,6 @@ __docformat__ = "restructuredtext en"
 __author__ = "John Westbrook"
 __email__ = "john.westbrook@rcsb.org"
 __license__ = "Apache 2.0"
-__version__ = "0.55"
+__version__ = "0.56"
 
 __apiUrl__ = "https://mmcif.wwpdb.org"
